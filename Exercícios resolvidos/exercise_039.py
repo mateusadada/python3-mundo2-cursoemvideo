a5a540f5b43a1203468c5fc4daa6143b1dfaaf83
Mateus Adada <mateusadada@gmail.com>
2023-06-30 18:28:02 -0300

    Update exercise_039.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_039.py
+++ Exercícios resolvidos/exercise_039.py
@@ -2,4 +2,16 @@
 # alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa
 # também deverá mostrar o tempo que falta ou que passou do prazo.
 
-# print('Bem-vindo ao programa de cálculo para o alistamento militar!')
+from datetime import date
+
+print('Bem-vindo ao programa de cálculo para o alistamento militar!')
+
+ano_de_nascimento = int(input('Informe o ano de nascimento: '))
+idade = date.today().year - ano_de_nascimento
+
+if idade < 18:
+    print(f'\n\033[32mVocê ainda vai se alistar no serviço militar! Falta(m) {18 - idade} ano(s)')
+elif idade == 18:
+    print(f'\n\033[33mVocê está no ano de se alistar no serviço militar!')
+else:
+    print(f'\n\033[31mVocê já passou do prazo de se alistar no serviço militar a {idade - 18} ano(s)')
