abdcf5369544462938e91f024a4cc01d8c2becd1
Mateus Adada <mateusadada@gmail.com>
2023-06-30 18:09:53 -0300

    Update exercises 036 and 038
    036: removed '#' in first print
    
    038: added the code and a note about function

--- Exercícios resolvidos/exercise_036.py
+++ Exercícios resolvidos/exercise_036.py
@@ -2,7 +2,7 @@
 # do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o
 # empréstimo será negado.
 
-# print('Bem-vindo ao programa de aprovação de empréstimo de uma casa!')
+print('Bem-vindo ao programa de aprovação de empréstimo de uma casa!')
 
 valor_da_casa = float(input('Valor da casa: R$ '))
 salario = float(input('Salário do comprador: R$ '))
--- Exercícios resolvidos/exercise_038.py
+++ Exercícios resolvidos/exercise_038.py
@@ -3,4 +3,14 @@
 # - O segundo valor é maior
 # - Não existe valor maior, os dois são iguais
 
-# print('Bem-vindo ao programa que compara dois números!')
+print('Bem-vindo ao programa que compara dois números inteiros!')
+
+numero1 = int(input('1º número: '))
+numero2 = int(input('2º número: '))
+
+if numero1 > numero2:
+    print(f'\nO primeiro valor \033[33m({numero1})\033[m é maior')
+elif numero2 > numero1:
+    print(f'\nO segundo valor \033[33m({numero2})\033[m é maior')
+else:
+    print(f'\nNão existe valor maior, os dois são iguais \033[33m({numero1})\033[m')
