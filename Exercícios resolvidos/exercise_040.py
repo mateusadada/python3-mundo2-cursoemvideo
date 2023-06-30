f61edc7e7e70fe59aa263befdc7ed90304b47021
Mateus Adada <mateusadada@gmail.com>
2023-06-30 18:37:42 -0300

    Update exercise_040.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_040.py
+++ Exercícios resolvidos/exercise_040.py
@@ -4,4 +4,16 @@
 # - Média entre 5.0 e 6.9: RECUPERAÇÃO
 # - Média 7.0 ou superior: APROVADO
 
-# print('Bem-vindo ao programa de cálculo da média!')
+print('Bem-vindo ao programa de cálculo da média!')
+
+nota1 = float(input('1º nota: '))
+nota2 = float(input('2º nota: '))
+
+media = (nota1 + nota2) / 2
+
+if media >= 7:
+    print(f'\n\033[32mAPROVADO!\033[m Média {media:.1f}')
+elif media < 5:
+    print(f'\n\033[31mREPROVADO!\033[m Média {media:.1f}')
+else:
+    print(f'\n\033[33mRECUPERAÇÃO!\033[m Média {media:.1f}')
