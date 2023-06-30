cb612be1c60d6478863e093f09c98531690d0f1c
Mateus Adada <mateusadada@gmail.com>
2023-06-30 18:04:45 -0300

    Update exercise_036.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_036.py
+++ Exercícios resolvidos/exercise_036.py
@@ -2,4 +2,17 @@
 # do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o
 # empréstimo será negado.
 
-# print('Bem-vindo ao programa de cálculo de empréstimo!')
+# print('Bem-vindo ao programa de aprovação de empréstimo de uma casa!')
+
+valor_da_casa = float(input('Valor da casa: R$ '))
+salario = float(input('Salário do comprador: R$ '))
+anos = int(input('Quantos anos que vai ser pago: '))
+
+prestacao_mensal = valor_da_casa / anos
+
+if prestacao_mensal / salario > 0.3:
+    print(f'\n\033[31mEmpréstimo NEGADO!\033[m Valor da prestação mensal acima do limite '
+          f'({prestacao_mensal / salario * 100:.1f}%)')
+else:
+    print(f'\n\033[32mEmpréstimo APROVADO!\033[m Valor da prestação mensal abaixo do limite '
+          f'({prestacao_mensal / salario * 100:.1f}%)')
