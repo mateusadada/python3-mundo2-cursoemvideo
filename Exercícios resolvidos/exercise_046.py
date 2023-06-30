e04d8b632dfdd9fc8ad544493a667de176aa6ccc
Mateus Adada <mateusadada@gmail.com>
2023-06-29 22:13:31 -0300

    Created exercises 36 to 71
    Added the code objetived

---
+++ Exercícios resolvidos/exercise_046.py
@@ -0,0 +1,2 @@
+# Faça um programa que mostre uma contagem regressiva na tela para o estouro de fogos de artifício, indo de 10 até 0,
+# com uma pausa de 1 segundo entre eles.
---
+++ Exercícios resolvidos/exercise_066.py
@@ -0,0 +1,3 @@
+# Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999,
+# sendo a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas
+# (desconsiderando o flag).
