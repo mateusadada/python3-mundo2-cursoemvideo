781336d77107e1037bb85fdf212cf0db1c069c4f
Mateus Adada <mateusadada@gmail.com>
2023-06-30 22:57:48 -0300

    Update exercise_043.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_043.py
+++ Exercícios resolvidos/exercise_043.py
@@ -6,4 +6,23 @@
 # - 30 até 40: Obesidade
 # - Acima de 40: Obesidade Mórbida
 
-# print('Bem-vindo ao programa de cálculo de IMC!')
+print('Bem-vindo ao programa de cálculo de IMC!')
+
+peso = float(input('Informe o peso (kg): '))
+altura = float(input('Altura (mt): '))
+
+imc = peso / altura ** 2
+
+print(f'\nIMC: \033[33m{imc:.1f}\033[m'
+      f'\nSituação: ', end='')
+
+if imc < 18.5:
+    print('\033[31mAbaixo do peso')
+elif 18.5 <= imc < 25:
+    print('\033[32mPeso ideal')
+elif 25 <= imc < 30:
+    print('\033[33mSobrepeso')
+elif 30 <= imc < 40:
+    print('\033[31mObesidade')
+else:
+    print('\033[31mObesidade mórbida')
