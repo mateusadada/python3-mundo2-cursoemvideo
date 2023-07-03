345caa708402fa888d9cf6abae4e7fded30b2359
Mateus Adada <mateusadada@gmail.com>
2023-07-02 23:08:13 -0300

    Update exercise_039.py
    Added age

--- Exercícios resolvidos/exercise_039.py
+++ Exercícios resolvidos/exercise_039.py
@@ -11,8 +11,10 @@ ano_de_nascimento = int(input('Informe o ano de nascimento: '))
 idade = date.today().year - ano_de_nascimento
 
 if idade < 18:
-    print(f'\n\033[32mVocê ainda vai se alistar no serviço militar! Falta(m) {18 - idade} ano(s)')
+    print(f'\n\033[32mVocê ainda vai se alistar no serviço militar!'
+          f'\nFalta(m) {18 - idade} ano(s); em {18 - idade + date.today().year}')
 elif idade == 18:
-    print(f'\n\033[33mVocê está no ano de se alistar no serviço militar!')
+    print(f'\n\033[33mVocê está no ano de se alistar no serviço militar (18 anos)!')
 else:
-    print(f'\n\033[31mVocê já passou do prazo de se alistar no serviço militar a {idade - 18} ano(s)')
+    print(f'\n\033[31mVocê já passou do prazo de se alistar no serviço militar a {idade - 18} ano(s); em '
+          f'{date.today().year - (idade - 18)}')
