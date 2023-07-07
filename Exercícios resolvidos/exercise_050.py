f26f286fe88734a2bd07fc43c7acae3d6815e2df
Mateus Adada <mateusadada@gmail.com>
2023-07-06 22:03:19 -0300

    Update exercise_050.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_050.py
+++ Exercícios resolvidos/exercise_050.py
@@ -1,4 +1,14 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor
 # digitado for ímpar, desconsidere-o.
 
-# print('Bem-vindo ao programa de cálculo da soma de seis números pares')
+print('Bem-vindo ao programa de cálculo da soma de seis números pares!')
+
+soma_pares = 0
+
+for i in range(6):
+    numero = int(input(f'{i + 1}º número: '))
+
+    if numero % 2 == 0:
+        soma_pares += numero
+
+print(f'\n\33[33mSoma dos números pares: {soma_pares}')
