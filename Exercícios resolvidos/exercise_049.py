e8ddb198014bac738e3599af5cc6cb2904c742c2
Mateus Adada <mateusadada@gmail.com>
2023-07-06 21:58:03 -0300

    Update exercise_049.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_049.py
+++ Exercícios resolvidos/exercise_049.py
@@ -1,3 +1,10 @@
 # Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
 
-# print('Bem-vindo ao programa de cálculo de tabuada!')
+print('Bem-vindo ao programa de cálculo da tabuada de um número qualquer!')
+
+numero = int(input('Digite um número: '))
+
+print(f'\n\33[33m*** TABUADA DO {numero} ***\n')
+
+for i in range(1, 11):
+    print(f'{numero} x {i} = {numero * i}')
