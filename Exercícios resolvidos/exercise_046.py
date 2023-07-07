8d0128d06ad7a7a8c5fa5ad9624ccb815868a222
Mateus Adada <mateusadada@gmail.com>
2023-07-06 21:42:09 -0300

    Update exercise_046.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_046.py
+++ Exercícios resolvidos/exercise_046.py
@@ -1,4 +1,14 @@
 # Faça um programa que mostre uma contagem regressiva na tela para o estouro de fogos de artifício, indo de 10 até 0,
 # com uma pausa de 1 segundo entre eles.
 
-# print('Bem-vindo ao programa de contagem regressiva!')
+from time import sleep
+
+print('Bem-vindo ao programa de contagem regressiva de fogos de artifício!'
+      '\n\n\33[32m*** CONTAGEM REGRESSIVA ***\n')
+
+for i in range(10, -1, -1):
+    sleep(1)
+    print('\33[33m', i)
+
+sleep(1)
+print('\n\33[31mESTOURAR OS FOGOS!!!')
