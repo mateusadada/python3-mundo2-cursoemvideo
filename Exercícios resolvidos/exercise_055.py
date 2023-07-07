778c2133f5f265640f90f4a3c8626c473a0702bf
Mateus Adada <mateusadada@gmail.com>
2023-07-06 23:04:05 -0300

    Update exercise_055.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_055.py
+++ Exercícios resolvidos/exercise_055.py
@@ -1,3 +1,22 @@
 # Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 
-# print('Bem-vindo ao programa de cálculo do maior e menor peso de cinco pessoas!')
+print('Bem-vindo ao programa de cálculo do maior e menor peso de cinco pessoas!')
+
+flag = 0
+
+for i in range(5):
+    peso = float(input(f'{i + 1}º peso: '))
+
+    if flag == 0:
+        flag = 1
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+
+        if peso < menor:
+            menor = peso
+
+print(f'\nMaior peso: \033[33m{maior:.1f} kg\033[m'
+      f'\nMenor peso: \033[33m{menor:.1f} kg\033[m')
