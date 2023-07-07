12bba7d9d2bb99dfda96d4e0992e8c2bdbb77d95
Mateus Adada <mateusadada@gmail.com>
2023-07-06 21:53:05 -0300

    Update exercise_047.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_047.py
+++ Exercícios resolvidos/exercise_047.py
@@ -1,3 +1,7 @@
 # Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
-# print('Bem-vindo ao programa que exibe todos os números pares entre 1 e 50!')
+print('Bem-vindo ao programa que exibe todos os números pares entre 1 e 50!\n\33[33m')
+
+for i in range(1, 51):
+    if i % 2 == 0:
+        print(i, end=', ')
