5a09138db2c9a9fbb9dc0860410a6bf9b80dd0ca
Mateus Adada <mateusadada@gmail.com>
2023-07-06 22:25:13 -0300

    Update exercise_051.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_051.py
+++ Exercícios resolvidos/exercise_051.py
@@ -1,4 +1,12 @@
 # Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa
 # progressão.
 
-# print('Bem-vindo ao programa de cálculo de uma PA!')
+print('Bem-vindo ao programa de cálculo de uma PA!')
+
+primeiro_termo = int(input('Digite o 1º termo de uma PA: '))
+razao = int(input('Razão: '))
+
+print()
+
+for i in range(10):
+    print(f'{i + 1}º termo: \33[33m{primeiro_termo + (razao * i)}\33[m')
