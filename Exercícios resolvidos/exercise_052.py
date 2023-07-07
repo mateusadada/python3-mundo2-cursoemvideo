28f358870337bceebfcbeaa964b6fb4804ad5c6b
Mateus Adada <mateusadada@gmail.com>
2023-07-06 22:44:14 -0300

    Update exercise_052.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_052.py
+++ Exercícios resolvidos/exercise_052.py
@@ -1,3 +1,15 @@
 # Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
 
-# print('Bem-vindo ao programa que verifica se um número é primo!')
+print('Bem-vindo ao programa que verifica se um número é primo!')
+
+numero = int(input('Digite um número natural: '))
+
+if numero < 2:
+    print('\n\33[31mInválido! O número precisa ser acima de 1!')
+else:
+    for i in range(2, numero):
+        if numero % i == 0:
+            print(f'\n\33[33mO número {numero} não é primo!')
+            break
+    else:
+        print(f'\n\33[32mO número {numero} é primo!')
