7bb7de2a36e1beff8b66aea5493573397f860289
Mateus Adada <mateusadada@gmail.com>
2023-07-10 16:59:39 -0300

    Update exercise_054.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_054.py
+++ Exercícios resolvidos/exercise_054.py
@@ -1,4 +1,21 @@
 # Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a
-# maioridade e quantas já são maiores.
+# maioridade e quantas já são maiores (>= 18).
 
-# print('Bem-vindo ao programa de cálculo de maioridade!')
+from datetime import date
+
+print('Bem-vindo ao programa de cálculo de maioridade (>= 18) de sete pessoas!')
+
+total_maioridade = 0
+total_minoridade = 0
+
+for i in range(7):
+    ano_de_nascimento = int(input(f'{i + 1}º pessoa: '))
+    idade = int(date.today().year - ano_de_nascimento)
+
+    if idade >= 18:
+        total_maioridade += 1
+    else:
+        total_minoridade += 1
+
+print(f'\n\033[33mMaioridade: {total_maioridade}'
+      f'\nMinoridade: {total_minoridade}')
