16bed44169961c0d6f277429048950c3a1577704
Mateus Adada <mateusadada@gmail.com>
2023-07-10 22:57:52 -0300

    Update exercise_056.py
    Added strip() and upper()

--- Exercícios resolvidos/exercise_056.py
+++ Exercícios resolvidos/exercise_056.py
@@ -10,13 +10,13 @@ maior_idade_homem = None
 nome_mais_velho_homem = None
 
 for i in range(4):
-    nome = str(input(f'\nNome da {i + 1}º pessoa: '))
+    nome = str(input(f'\nNome da {i + 1}º pessoa: ')).strip()
     idade = int(input('Idade: '))
-    sexo = int(input('Sexo (1-F / 2-M): '))
+    sexo = str(input('Sexo (F ou M): ')).strip().upper()
 
     total_idade += idade
 
-    if sexo == 2:
+    if sexo == 'M':
         if flag == 0:
             flag = 1
             maior_idade_homem = idade
@@ -26,12 +26,12 @@ for i in range(4):
             maior_idade_homem = idade
             nome_mais_velho_homem = nome
 
-    elif sexo == 1:
+    elif sexo == 'F':
         if idade < 20:
             idade_mulher_menos_20 += 1
 
 media_idade = total_idade / 4
 
-print(f'\n\033[33mA média de idade do grupo é {media_idade:.0f} anos'
+print(f'\n\033[33mA média de idade do grupo é {media_idade:.1f} anos'
       f'\nNome do homem mais velho: {nome_mais_velho_homem}'
       f'\n{idade_mulher_menos_20} mulher(es) têm menos de 20 anos')
