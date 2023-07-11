3e8d482c66965e7571e953362e53f07657ef1ce8
Mateus Adada <mateusadada@gmail.com>
2023-07-10 21:02:24 -0300

    Update exercise_048.py
    Improved for

--- Exercícios resolvidos/exercise_048.py
+++ Exercícios resolvidos/exercise_048.py
@@ -1,11 +1,11 @@
-# Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo
-# de 1 até 500.
+# Faça um programa que calcule a soma entre todos os números ímpares e que são múltiplos de três e que se encontram no
+# intervalo de 1 até 500.
 
 print('Bem-vindo ao programa de cálculo da soma de todos os múltiplos de três entre 1 e 500!')
 
 total = 0
 
-for i in range(1, 501):
+for i in range(1, 501, 2):
     if i % 3 == 0:
         total += i
 
