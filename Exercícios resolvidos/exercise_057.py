af354384b2fe315f116438f1d59dac2218ee6959
Mateus Adada <mateusadada@gmail.com>
2023-07-11 19:49:36 -0300

    Update exercise_057.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_057.py
+++ Exercícios resolvidos/exercise_057.py
@@ -1,4 +1,12 @@
 # Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a
 # digitação novamente até ter um valor correto.
 
-# print('Bem-vindo ao programa de que permite apenas entradas de "M" e "F"!')
+print('Bem-vindo ao programa que permite apenas entradas de "M" ou "F"!')
+
+sexo = str(input('Digite o sexo (M ou F): ')).strip().upper()
+
+while sexo not in 'MmFf':
+    print('\033[31mEntrada inválida! Tente novamente...\033[m\n')
+    sexo = str(input('Digite o sexo (M ou F): ')).strip().upper()
+
+print(f'\n\033[33mO sexo digitado foi {sexo}')
