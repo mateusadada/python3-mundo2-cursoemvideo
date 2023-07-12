c50dfde628e349fde65262f465a981684d6ad46f
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-07-12 15:54:14 -0300

    Update exercise_064.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_064.py
+++ Exercícios resolvidos/exercise_064.py
@@ -2,4 +2,17 @@
 # valor 999, sendo a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles
 # (desconsiderando o flag).
 
-# print('Bem-vindo ao programa de cálculo da soma de N numeros!')
+print('Bem-vindo ao programa de cálculo da soma de N números!')
+
+numero = 0
+total = 0
+contador = 0
+
+while numero != 999:
+    numero = int(input('Digite um número inteiro (999 p/ sair): '))
+
+    if numero != 999:
+        contador += 1
+        total += numero
+
+print(f'\n\033[33mForam digitados {contador} número(s) resultando na soma de {total}')
