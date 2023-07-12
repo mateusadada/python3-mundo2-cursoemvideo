65b20f18810b3687193af545add7e8a60ecd1d5e
Mateus Adada <mateusadada@gmail.com>
2023-07-11 21:42:46 -0300

    Update exercise_061.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_061.py
+++ Exercícios resolvidos/exercise_061.py
@@ -1,4 +1,15 @@
 # Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão
 # usando a estrutura while.
 
-# print('Bem-vindo ao programa de cálculo de uma PA!')
+print('Bem-vindo ao programa de cálculo de uma PA!')
+
+primeiro_termo = int(input('Digite o 1º termo de uma PA: '))
+razao = int(input('Razão: '))
+
+print()
+
+contador = 0
+
+while contador < 10:
+    print(f'{contador + 1}º termo: \033[33m{primeiro_termo + (razao * contador)}\033[m')
+    contador += 1
