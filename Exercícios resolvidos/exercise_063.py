10925bb040ae83a8c351310ecc2ba1a6516a6e0e
Mateus Adada <mateusadada@gmail.com>
2023-07-11 22:38:28 -0300

    Update exercise_063.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_063.py
+++ Exercícios resolvidos/exercise_063.py
@@ -2,4 +2,25 @@
 # de Fibonacci.
 # Exemplo: 0 - 1 - 1 - 2 - 3 - 5 - 8
 
-# print('Bem-vindo ao programa de cálculo de uma Sequência de Fibonacci!')
+print('Bem-vindo ao programa de cálculo de uma Sequência de Fibonacci!')
+
+sequencia = int(input('Digite a quantidade de elementos para listar: '))
+
+numero1 = 0
+numero2 = 1
+
+contador = 0
+
+print('\n\033[33m0', end=' - ')
+
+while contador < sequencia - 1:
+    if contador + 1 == sequencia - 1:
+        print(numero2)
+    else:
+        print(numero2, end=' - ')
+
+    proximo_numero = numero1 + numero2
+    numero1 = numero2
+    numero2 = proximo_numero
+
+    contador += 1
