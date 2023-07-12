d53e9fdd04850e15dd350aa9b81635654e0a253e
Mateus Adada <mateusadada@gmail.com>
2023-07-11 22:15:49 -0300

    Update exercise_062.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_062.py
+++ Exercícios resolvidos/exercise_062.py
@@ -1,4 +1,21 @@
 # Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando
 # ele disser que quer mostrar 0 termos.
 
-# print('Bem-vindo ao programa de cálculo de n PA!')
+# print('Bem-vindo ao programa de cálculo de n números de uma PA!')
+
+print('Bem-vindo ao programa de cálculo de uma PA!')
+
+primeiro_termo = int(input('Digite o 1º termo de uma PA: '))
+razao = int(input('Razão: '))
+
+contador = 0
+quantidade_termos = 1
+
+while quantidade_termos != 0:
+    quantidade_termos = int(input('Mostrar quantos termos (0 p/ sair)? '))
+
+    while contador < quantidade_termos:
+        print(f'{contador + 1}º termo: \033[33m{primeiro_termo + (razao * contador)}\033[m')
+        contador += 1
+
+    print()
