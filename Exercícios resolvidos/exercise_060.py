ac13ecd7c1401e272758a2df1cff02e9d66a37d9
Mateus Adada <mateusadada@gmail.com>
2023-07-11 21:36:55 -0300

    Update exercise_060.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_060.py
+++ Exercícios resolvidos/exercise_060.py
@@ -1,4 +1,28 @@
 # Faça um programa que leia um número qualquer e mostre o seu fatorial.
 # Exemplo: 5! = 5 x 4 x 3 x 2 x 1 = 120
 
-# print('Bem-vindo ao programa de cálculo de fatorial!')
+print('Bem-vindo ao programa de cálculo de fatorial!')
+
+numero = 0
+
+while numero < 1:
+    numero = int(input('Digite um número qualquer positivo: '))
+
+    if numero < 1:
+        print('\033[31mEntrada inválida! Tente novamente.\033[m\n')
+
+total = 1
+
+print(f'\n\033[33mFatorial de {numero}! = ', end='')
+
+while numero > 0:
+    total *= numero
+
+    if numero == 1:
+        print(numero, end=' = ')
+    else:
+        print(numero, end=' x ')
+
+    numero -= 1
+
+print(total)
