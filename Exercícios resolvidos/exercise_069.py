32a1ed551d91639aeb295ddbd340203c623e3f01
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-07-13 11:54:17 -0300

    Update exercise_069.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_069.py
+++ Exercícios resolvidos/exercise_069.py
@@ -4,4 +4,44 @@
 # B) quantos homens foram cadastrados.
 # C) quantas mulheres tem menos de 20 anos.
 
-# print('Bem-vindo ao programa de cálculo a partir da idade e do sexo de N pessoas!')
+from time import sleep
+
+pessoa_maior_18 = homem = mulher_menos_20 = 0
+
+print('Bem-vindo ao programa de cálculo a partir da idade e do sexo de N pessoas!', end='')
+
+while True:
+    idade = int(input('\nDigite a idade: '))
+
+    # validação do sexo
+    while True:
+        sexo = str(input('Digite o sexo [M/F]: ')).strip().upper()[0]
+
+        if sexo not in 'MF':
+            print('\033[31mEntrada inválida! Tente novamente.\033[m')
+        else:
+            break
+
+    # inclusão dos três critérios
+    if idade > 18:
+        pessoa_maior_18 += 1
+
+    if sexo == 'M':
+        homem += 1
+
+    if sexo == 'F' and idade < 20:
+        mulher_menos_20 += 1
+
+    # validação da continuação do programa
+    continuar = str(input('Deseja continuar [S/N]? ')).strip().upper()[0]
+
+    if continuar == 'N':
+        # exibição do resultado
+        print(f'\nPessoas que têm mais de 18 anos: {pessoa_maior_18}'
+              f'\nHomens que foram cadastrados: {homem}'
+              f'\nMulheres que tem menos de 20 anos: {mulher_menos_20}')
+
+        print('\n\033[33mSaindo do programa...')
+        sleep(2)
+        print('Programa finalizado. Obrigado por utilizar. Até logo!')
+        break
